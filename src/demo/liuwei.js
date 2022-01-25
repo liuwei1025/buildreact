825f148ce0d298ba40329cebe1d6304007439b91
刘威 <liuwei89@jd.com>
2022-01-25 11:00:46 +0800

    feat: 增加demo文件

--- src/demo/liuwei.js
+++ src/demo/liuwei.js
@@ -1,5 +1,36 @@
 const Didact = {
-    createElement() {},
-    // hi
-    render() {}
+  createElement,
+  render,
+};
+
+function render(element, container) {
+  const dom = document.createElement(element.type);
+  Object.keys(element.props || {}).forEach(key => {
+    if (key !== 'children') {
+      dom.setAttribute(key, element.props[key]);
+    }
+  });
+  element.props?.children?.map(child => {
+    render(createElement(child), dom);
+  });
+  container.appendChild(dom);
 }
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+}
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
+
+Didact.render(element, document.querySelector('#liuwei'));
