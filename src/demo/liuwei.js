7fc90dcc2fb244cfe4ca7eb364f5261e0238fa69
刘威 <liuwei89@jd.com>
2022-02-16 18:30:39 +0800

    feat: 完成fiber构造

--- src/demo/liuwei.js
+++ src/demo/liuwei.js
@@ -19,24 +19,80 @@ function createTextElement(text) {
       nodeValue: text,
       /**
        * React doesn’t wrap primitive values or create empty arrays when there aren’t children,
-       * but we do it because it will simplify our code, and for our library we prefer simple code than performant code.
+       * but we do it because it will simplify our code,
+       * and for our library we prefer simple code than performant code.
        */
       children: [],
     },
   };
 }
-function render(element, container) {
-  const dom = element.type == 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type);
-
+function createDom(fiber) {
+  const dom = fiber.type == 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type);
   const isProperty = key => key !== 'children';
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(name => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-    
-  element.props.children.forEach(child => render(child, dom));
-  container.appendChild(dom);
+  return dom;
+}
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 const Didact = {
   createElement,
@@ -46,7 +102,15 @@ const Didact = {
 const element = (
   <div id="foo">
     <a>bar</a>
-    <b />
+    <div>
+      <h1>
+        <p>123</p>
+        <a>23</a>
+      </h1>
+      <h2>
+        <b></b>
+      </h2>
+    </div>
   </div>
 );
 console.log(element);
