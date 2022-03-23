c9024d1dc59834e5763584e621eb2a8926e5725b
刘威 <liuwei89@jd.com>
2022-03-23 14:46:08 +0800

    feat: 模拟页面卡顿的情况 我们会得到一个局部渲染的UI

--- src/demo/liuwei.js
+++ src/demo/liuwei.js
@@ -47,6 +47,16 @@ function render(element, container) {
 
 let nextUnitOfWork = null;
 
+window.requestIdleCallback = function (fn) {
+  setTimeout(() => {
+    fn({
+      timeRemaining() {
+        return 0;
+      },
+    });
+  }, 500);
+};
+
 function workLoop(deadline) {
   let shouldYield = false;
   while (nextUnitOfWork && !shouldYield) {
@@ -100,17 +110,114 @@ const Didact = {
 };
 /** @jsx Didact.createElement */
 const element = (
-  <div id="foo">
-    <a>bar</a>
-    <div>
-      <h1>
-        <p>123</p>
-        <a>23</a>
-      </h1>
-      <h2>
-        <b></b>
-      </h2>
-    </div>
+  <div id="wmd-preview" class="wmd-preview" data-medium-element="true" style="height: auto;">
+    <h1 data-anchor-id="nrlj" id="linux操作">
+      linux操作
+    </h1>
+    <p data-anchor-id="thek">
+      <code>系统</code>
+    </p>
+    <hr />
+    <div class="md-section-divider"></div>
+    <h2 data-anchor-id="x4dp" id="linux基本操作">
+      <a href="http://blog.51cto.com/liguxk/152912" target="_blank">
+        linux基本操作
+      </a>
+    </h2>
+    <ul data-anchor-id="tw1a">
+      <li>
+        <code>sudo -i</code> 进入<code>root</code>用户
+      </li>
+      <li>
+        <code>exit</code> 从root用户返回到之前的用户
+      </li>
+      <li>
+        查看硬件信息 <code>df -h</code>
+      </li>
+      <li>
+        <code>lsof -i tcp:3306</code>查看端口占用并返回pid，<code>list open file</code>的缩写
+      </li>
+      <li>
+        <a href="https://www.tecmint.com/find-out-which-process-listening-on-a-particular-port/" target="_blank">
+          查看占用端口的程序
+        </a>
+        <code>netstat -ltnp | grep -w ':80'</code>
+      </li>
+      <li>
+        <a href="https://www.plesk.com/blog/various/find-files-in-linux-via-command-line/" target="_blank">
+          查找文件
+        </a>
+        <code>find /home/ocean -name "index.html"</code>
+      </li>
+    </ul>
+    <div class="md-section-divider"></div>
+    <h3 data-anchor-id="cj8k" id="系统">
+      系统
+    </h3>
+    <ul data-anchor-id="zc3h">
+      <li>
+        <code>uname -a</code> 查看内核/操作系统/CPU信息
+      </li>
+      <li>
+        <code>cat /proc/cupinfo</code>查看CPU信息
+      </li>
+      <li>
+        <code>env</code>查看环境变量
+      </li>
+      <li>
+        <code>free -m</code>查看内存使用量和交换区使用量
+      </li>
+      <li>
+        <code>df -h</code>查看各分区使用情况
+      </li>
+      <li>
+        <code>head -n 1 /etc/issue</code> 查看操作系统版本
+      </li>
+      <li>
+        <code>sudo -i</code>输入密码进入root
+      </li>
+      <li>
+        <code>exit</code>退出root
+      </li>
+      <li>
+        <code>ps -ef | grep node</code>
+        <a href="https://linuxtools-rst.readthedocs.io/zh_CN/latest/tool/ps.html" target="_blank">
+          ps
+        </a>
+      </li>
+    </ul>
+    <div class="md-section-divider"></div>
+    <h3 data-anchor-id="rlak" id="网络">
+      网络
+    </h3>
+    <p></p>
+    <ul data-anchor-id="mjvh">
+      <li>
+        <code>netstat -an|grep 8088</code> 查看端口占用
+      </li>
+      <li>
+        <code>lsof -i:8088</code> <br />
+        <p></p>
+        <blockquote class="white-blockquote">
+          通过list open file命令可以查看到当前打开文件，在linux中所有事物都是以文件形式存在，包括网络连接及硬件设备
+          <code>-i</code>参数表示网络链接，<code>:8088</code>指明端口号，该命令会同时列出<code>PID</code>，方便kill
+        </blockquote>
+      </li>
+      <li>
+        <code>sudo lsof -i -P | grep -i "listen"</code> 产看所有进程监听的端口
+      </li>
+      <li>
+        <code>nc -zv 127.0.0.1 80</code> 查看IP和端口是否能连接上
+      </li>
+      <li>
+        <code>arp -d -a</code>
+        <a href="https://wangchujiang.com/linux-command/c/arp.html" target="_blank">
+          删除网络缓存
+        </a>
+      </li>
+    </ul>
+    <div class="md-section-divider"></div>
+    <h3 data-anchor-id="a7wn" id="title"></h3>
   </div>
 );
 console.log(element);
